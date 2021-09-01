bc1053e6996d4591faf09b4827c93b5842dcdfd1
Laura Londoño <89430217+LauraLondono@users.noreply.github.com>
2021-09-01 16:41:59 -0700

    Update AudioPlayer.js

--- PARCIAL FRONTEND - Londoño 55361 y Garzón 55134/src/AudioPlayer.js
+++ PARCIAL FRONTEND - Londoño 55361 y Garzón 55134/src/AudioPlayer.js
@@ -3,13 +3,13 @@ const playPause = document.querySelector("#icon");
 
 playPause.addEventListener("click", () => {
   if (audio.paused || audio.ended) {
-    playPause.querySelector("#pause").classList.remove("hide");
     playPause.querySelector("#play").classList.add("hide");
+    playPause.querySelector("#pause").classList.remove("hide");    
     audio.play();
   } else {
     audio.pause();
-    playPause.querySelector("#pause").classList.add("hide");
     playPause.querySelector("#play").classList.remove("hide");
+    playPause.querySelector("#pause").classList.add("hide");    
   }
 });
 
