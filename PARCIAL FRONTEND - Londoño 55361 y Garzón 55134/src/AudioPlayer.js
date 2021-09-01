7e4d8e4a4a54deda126916b1c66c42fb7a0ac8b0
Laura Londoño <89430217+LauraLondono@users.noreply.github.com>
2021-08-31 21:17:27 -0700

    Add files via upload

---
+++ PARCIAL FRONTEND - Londoño 55361 y Garzón 55134/src/AudioPlayer.js
@@ -0,0 +1,36 @@
+const audio = document.querySelector("#song");
+const playPause = document.querySelector("#icon");
+
+playPause.addEventListener("click", () => {
+  if (audio.paused || audio.ended) {
+    playPause.querySelector("#pause").classList.remove("hide");
+    playPause.querySelector("#play").classList.add("hide");
+    audio.play();
+  } else {
+    audio.pause();
+    playPause.querySelector("#pause").classList.add("hide");
+    playPause.querySelector("#play").classList.remove("hide");
+  }
+});
+
+/*
+playPause.addEventListener("click", () => {
+  if (audio.paused || audio.ended) {
+    pause();
+  } else {
+    play();
+  }
+});
+
+const pause = () => {
+  playPause.querySelector("#pause").classList.remove("hide");
+  playPause.querySelector("#play").classList.add("hide");
+  audio.play();
+}
+
+const play = () => {
+  audio.pause();
+  playPause.querySelector("#pause").classList.add("hide");
+  playPause.querySelector("#play").classList.remove("hide");
+}
+*/
